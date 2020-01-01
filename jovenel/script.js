7b3b59b293ed099b1188b5f8712d0813270abf93
JS-jopepe <jovenel.s@outlook.com>
2020-01-01 13:22:58 -0300

    Criando o projeto e o app

---
+++ jovenel/script.js
@@ -0,0 +1,12 @@
+alert ('Hello and Welcome!!!')
+
+a = prompt('Qual é o seu nome?');
+b = Number(prompt('Digite s'));
+c = prompt(' Digite seu email')
+
+
+ if (b == 16 ) {
+     alert('Você é Jovenellllllllllll');
+ } else {
+     alert('Sorry :(((((((((((((((((( ');
+ }
